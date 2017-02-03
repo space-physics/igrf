fa7673b767a458134dd8e6e49437901498bc756e
scienceopen <scienceopen@users.noreply.github.com>
2017-02-03 00:52:35 -0500

    functionalize

---
+++ igrf12py/plots.py
@@ -0,0 +1,47 @@
+from matplotlib.pyplot import figure,subplots
+from matplotlib.ticker import ScalarFormatter
+#
+sfmt = ScalarFormatter(useMathText=True) #for 10^3 instead of 1e3
+sfmt.set_powerlimits((-2, 2))
+sfmt.set_scientific(True)
+sfmt.set_useOffset(False)
+
+def plotigrf(x,y,z,f,glat,glon,year,isv,mdl):
+    fg,ax = subplots(2,2,sharex=True)
+    ax = ax.ravel()
+    for a,i,j in zip(ax,(x,y,z),('x','y','z')):
+        hi = a.imshow(i,extent=(glon[0,0],glon[0,-1],glat[0,0],glat[-1,0]),
+                      cmap='bwr',
+                      vmin=-6e4,vmax=6e4) #symmetrix vmin,vmax centers white at zero for bwr cmap
+        fg.colorbar(hi,ax=a,format=sfmt)
+        a.set_title('IGRF{} $B_{}$-field on {:.3f}'.format(mdl,j,year))
+
+    for a in ax[[0,2]]:
+        a.set_ylabel('latitude (deg)')
+    for a in ax[[2,3]]:
+        a.set_xlabel('longitude (deg)')
+
+
+    if isv==0:
+        hi = a.imshow(f,extent=(glon[0,0],glon[0,-1],glat[0,0],glat[-1,0]))
+        fg.colorbar(hi,ax=a,format=sfmt)
+        a.set_title('IGRF{} $B$-field: total intensity [nT] on {:.2f}'.format(mdl,year))
+
+def plotdiff1112(x,x11,y,y11,z,z11,f,f11,glat,glon,year,isv):
+    for i,j,k in zip((x,y,z),(x11,y11,z11),('x','y','z')):
+        fg = figure()
+        ax = fg.gca()
+        hi = ax.imshow(i-j,extent=(glon[0,0],glon[0,-1],glat[0,0],glat[-1,0]))
+        fg.colorbar(hi,format=sfmt)
+        ax.set_ylabel('latitude (deg)')
+        ax.set_xlabel('longitude (deg)')
+        ax.set_title('IGRF12-IGRF11 $B_{}$-field comparison on {:.2f}'.format(k,year))
+
+    if isv==0:
+        fg = figure()
+        ax = fg.gca()
+        hi = ax.imshow(f-f11,extent=(glon[0,0],glon[0,-1],glat[0,0],glat[-1,0]))
+        fg.colorbar(hi)
+        ax.set_xlabel('latitude (deg)')
+        ax.set_ylabel('longitude (deg)')
+        ax.set_title('IGRF12-IGRF11 $B$-field: comparison total intensity [nT] on {:.2f}'.format(year))
