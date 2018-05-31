b5d276e72dcc568bb070ad599635fb69573a34ac
scivision <scivision@users.noreply.github.com>
2018-05-31 00:47:26 -0400

    pep8

--- RunIGRF.py
+++ RunIGRF.py
@@ -8,31 +8,38 @@ import pyigrf12.plots as plt
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
-    p = ArgumentParser(description='calls IGRF from Python, and plots the modeled geomagnetic field')
-    p.add_argument('date', help='date of sim',nargs='?',default=datetime.date.today())
-    p.add_argument('--isv',help='0: main field. 1: secular variation',type=int,default=0)
-    p.add_argument('--itype',help='1: geodetic. 2: geocentric',type=int,default=1)
-    p.add_argument('-a','--altkm',help='(km) above sea level if itype=1, (km) from center of Earth if itype=2',type=float,nargs='+',default=0)
-    p.add_argument('-c','--latlon',help='geodetic latitude, longitude (deg)',type=float,nargs=2)
+    p = ArgumentParser(description='calls IGRF from Python, and plots '
+                       'the modeled geomagnetic field')
+    p.add_argument('date', help='date of sim', nargs='?',
+                   default=datetime.date.today())
+    p.add_argument('--isv', help='0: main field. 1: secular variation',
+                   type=int, default=0)
+    p.add_argument('--itype', help='1: geodetic. 2: geocentric', type=int,
+                   default=1)
+    p.add_argument('-a', '--altkm',
+                   help='(km) above sea level if itype=1,'
+                        ' (km) from center of Earth if itype=2', type=float,
+                   nargs='+', default=0)
+    p.add_argument('-c', '--latlon', help='geodetic latitude, longitude (deg)',
+                   type=float, nargs=2)
     p = p.parse_args()
-
 
     # do world-wide grid if no user input
     if not p.altkm or not p.latlon:
-        glat,glon = pyigrf12.latlonworldgrid()
+        glat, glon = pyigrf12.latlonworldgrid()
     elif p.altkm and p.latlon:
-        glat,glon = p.latlon
+        glat, glon = p.latlon
     else:
         raise ValueError('please input all 3 of lat,lon,alt or none of them')
 
-    mag   = pyigrf12.gridigrf12(p.date,glat,glon,p.altkm, p.isv, p.itype)
+    mag12 = pyigrf12.gridigrf12(p.date, glat, glon, p.altkm, p.isv, p.itype)
 #   mag11 = pyigrf12.testigrf11(p.date,glat,glon,p.altkm, p.isv, p.itype)
 
-    if glat.ndim==2:
-        plt.plotigrf(mag,'12')
+    if glat.ndim == 2:
+        plt.plotigrf(mag12, '12')
 
-        #plt.plotdiff1112(mag,mag11)
+        # plt.plotdiff1112(mag12,mag11)
     else:
-        print(mag)
+        print(mag12)
 
     show()
--- pyigrf12/__init__.py
+++ pyigrf12/__init__.py
@@ -11,46 +11,48 @@ import sciencedates
 import igrf12
 
 
-def gridigrf12(date:datetime, glat:float, glon:float, alt:float, isv:int=0, itype:int=1) -> xarray.Dataset:
+def gridigrf12(date: datetime, glat: float, glon: float, alt: float,
+               isv: int=0, itype: int=1) -> xarray.Dataset:
 
     glat = np.atleast_1d(glat)
     glon = np.atleast_1d(glon)
 
     yeardec = sciencedates.datetime2yeardec(date)
-    colat,elon = latlon2colat(glat.ravel(), glon.ravel())
+    colat, elon = latlon2colat(glat.ravel(), glon.ravel())
 
     x = np.empty(colat.size)
-    y = np.empty_like(x);
+    y = np.empty_like(x)
     z = np.empty_like(x)
     f = np.empty_like(x)
 
-    for i,(clt,eln) in enumerate(zip(colat,elon)):
-        x[i],y[i],z[i],f[i] = igrf12.igrf12syn(isv, yeardec, itype, alt, clt, eln)
+    for i, (clt, eln) in enumerate(zip(colat, elon)):
+        x[i], y[i], z[i], f[i] = igrf12.igrf12syn(isv, yeardec, itype, alt,
+                                                  clt, eln)
 # %% assemble output
-    if glat.ndim==2 and glon.ndim==2: # assume meshgrid
-        coords={'glat':glat[:,0], 'glon':glon[0,:]}
-    elif glat.ndim==1 and glon.ndim==1:
-        coords={'glat':glat, 'glon':glon}
+    if glat.ndim == 2 and glon.ndim == 2:  # assume meshgrid
+        coords = {'glat': glat[:, 0], 'glon': glon[0, :]}
+    elif glat.ndim == 1 and glon.ndim == 1:
+        coords = {'glat': glat, 'glon': glon}
     else:
-        raise ValueError('unexpected glat/glon shapes {} {}'.format(glat.shape, glon.shape))
+        raise ValueError(f'glat/glon shapes: {glat.shape} {glon.shape}')
 
-
-    mag = xarray.Dataset(coords = coords,
-                         attrs={'time':date, 'isv':isv})
-    mag['north'] = (('glat','glon'), x.reshape(glat.shape))
-    mag['east'] = (('glat','glon'), y.reshape(glat.shape))
-    mag['down'] = (('glat','glon'), z.reshape(glat.shape))
-    mag['total'] = (('glat','glon'), f.reshape(glat.shape))
+    mag = xarray.Dataset(coords=coords,
+                         attrs={'time': date, 'isv': isv})
+    mag['north'] = (('glat', 'glon'), x.reshape(glat.shape))
+    mag['east'] = (('glat', 'glon'), y.reshape(glat.shape))
+    mag['down'] = (('glat', 'glon'), z.reshape(glat.shape))
+    mag['total'] = (('glat', 'glon'), f.reshape(glat.shape))
 
     decl, incl = mag_vector2incl_decl(mag.north, mag.east, mag.down)
 
-    mag['incl'] = (('glat','glon'),incl)
-    mag['decl'] = (('glat','glon'),decl)
+    mag['incl'] = (('glat', 'glon'), incl)
+    mag['decl'] = (('glat', 'glon'), decl)
 
     return mag
 
 
-def igrf(date:datetime, glat:float, glon:float, alt:float, isv:int=0, itype:int=1, model:str='12') -> xarray.Dataset:
+def igrf(date: datetime, glat: float, glon: float, alt: float,
+         isv: int=0, itype: int=1, model: str='12') -> xarray.Dataset:
     """
     date: datetime.date or decimal year yyyy.dddd
     glat, glon: geographic Latitude, Longitude
@@ -60,47 +62,49 @@ def igrf(date:datetime, glat:float, glon:float, alt:float, isv:int=0, itype:int=
     """
 
     # decimal year
-    if isinstance(date,(datetime.date, datetime.datetime)):
+    if isinstance(date, (datetime.date, datetime.datetime)):
         yeardec = sciencedates.datetime2yeardec(date)
-    elif isinstance(yeardec,float): # assume decimal year
+    elif isinstance(yeardec, float):  # assume decimal year
         pass
     else:
-        raise TypeError('unknown yeardec type {}'.format(type(yeardec)))
+        raise TypeError(f'unknown yeardec type {type(yeardec)}')
 
-    colat, elon = latlon2colat(glat,glon)
+    colat, elon = latlon2colat(glat, glon)
 
-    assert colat.size==elon.size==1
+    assert colat.size == elon.size == 1
 
     alt = np.atleast_1d(alt)
     Bnorth = np.empty(alt.size)
     Beast = np.empty_like(Bnorth)
     Bvert = np.empty_like(Bnorth)
     Btotal = np.empty_like(Bnorth)
-    for i,a in enumerate(alt):
-        if model=='12':
-            Bnorth[i], Beast[i], Bvert[i], Btotal[i] = igrf12.igrf12syn(isv, yeardec, itype, a, colat, elon)
-        elif model=='11':
+    for i, a in enumerate(alt):
+        if model == '12':
+            Bnorth[i], Beast[i], Bvert[i], Btotal[i] = igrf12.igrf12syn(isv,
+                  yeardec, itype, a,  colat, elon)  # noqa E128
+        elif model == '11':
             import igrf11
-            Bnorth[i], Beast[i], Bvert[i], Btotal[i] = igrf11.igrf11syn(isv, yeardec, itype, a, colat, elon)
+            Bnorth[i], Beast[i], Bvert[i], Btotal[i] = igrf11.igrf11syn(isv,
+                                             yeardec, itype, a, colat, elon)  # noqa E128
         else:
             raise ValueError('unknown IGRF model {}'.format(model))
 # %% assemble output
-    mag = xarray.Dataset({'north':('alt_km',Bnorth),
-
-                          'east':('alt_km',Beast),
-                          'down':('alt_km',Bvert),
-                          'total':('alt_km',Btotal)},
-                          coords={'alt_km':alt})
+    mag = xarray.Dataset({'north': ('alt_km', Bnorth),
+                          'east': ('alt_km', Beast),
+                          'down': ('alt_km', Bvert),
+                          'total': ('alt_km', Btotal)},
+                         coords={'alt_km': alt})
 
     decl, incl = mag_vector2incl_decl(mag.north, mag.east, mag.down)
 
-    mag['incl'] = ('alt_km',incl)
-    mag['decl'] = ('alt_km',decl)
+    mag['incl'] = ('alt_km', incl)
+    mag['decl'] = ('alt_km', decl)
 
     return mag
 
+
 # %% utility functions
-def mag_vector2incl_decl(x:float,y:float,z:float) -> tuple:
+def mag_vector2incl_decl(x: float, y: float, z: float) -> tuple:
     """
     Inputs:
     -------
@@ -117,22 +121,23 @@ def mag_vector2incl_decl(x:float,y:float,z:float) -> tuple:
     http://geomag.nrcan.gc.ca/mag_fld/comp-en.php
     """
 
-    decl = np.degrees(np.arctan2(y,x))
+    decl = np.degrees(np.arctan2(y, x))
 
-    incl = np.degrees(np.arctan2(z, np.hypot(x,y)))
+    incl = np.degrees(np.arctan2(z, np.hypot(x, y)))
 
     return decl, incl
 
-def latlon2colat(glat:float, glon:float):
-    #atleast_1d for iteration later
+
+def latlon2colat(glat: float, glon: float):
+    # atleast_1d for iteration later
     colat = 90 - np.atleast_1d(glat)
     elon = (360 + np.atleast_1d(glon)) % 360
 
-    return colat,elon
+    return colat, elon
 
 
-def latlonworldgrid(latstep:int=5, lonstep:int=5):
-    lat = np.arange(-90.,90+latstep,latstep)
-    lon = np.arange(-180.,180+lonstep,lonstep)
-    glon,glat = np.meshgrid(lon,lat)
-    return glat,glon
+def latlonworldgrid(latstep: int=5, lonstep: int=5):
+    lat = np.arange(-90., 90 + latstep, latstep)
+    lon = np.arange(-180., 180 + lonstep, lonstep)
+    glon, glat = np.meshgrid(lon, lat)
+    return glat, glon
