bed206a31500f090d8a02af586fcc8fa468938ce
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-06-04 13:02:41 -0400

    metadata [ci skip]

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-install_requires = ['xarray','numpy','sciencedates']
-tests_require=['pytest','nose','coveralls']
-
-# %%
 from setuptools import find_packages
-from numpy.distutils.core import setup,Extension
-#%% install
+from numpy.distutils.core import setup, Extension
+
+install_requires = ['xarray', 'numpy', 'sciencedates']
+tests_require = ['pytest', 'nose', 'coveralls']
+
+# %% install
 setup(name='pyigrf12',
       packages=find_packages(),
       version='1.3.1',
@@ -13,22 +13,23 @@ setup(name='pyigrf12',
       url='https://github.com/scivision/pyigrf12',
       description='IGRF12 model with simple object-oriented Python interface',
       long_description=open('README.rst').read(),
-	  install_requires=install_requires,
+          install_requires=install_requires,
       ext_modules=[Extension(name='igrf12',
-                           sources=['fortran/igrf12.f'],
-                           f2py_options=['--quiet'],
-                           extra_f77_compile_args=['-w'])],
+                             sources=['fortran/igrf12.f'],
+                             f2py_options=['--quiet'],
+                             extra_f77_compile_args=['-w'])],
       classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Intended Audience :: Science/Research',
           'License :: OSI Approved :: MIT License',
+          'Programming Language :: Fortran',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Topic :: Scientific/Engineering :: Atmospheric Science',
-          ],
-      extras_require={'plot':['matplotlib'],
-                       'tests':tests_require,},
+      ],
+      extras_require={'plot': ['matplotlib'],
+                      'tests': tests_require, },
       python_requires='>=3.5',
       tests_require=tests_require,
-	  )
+      )
